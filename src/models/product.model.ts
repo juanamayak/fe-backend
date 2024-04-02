de3eb0fa6605f0e898881a53d1fd27a1535f1847
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2024-04-02 16:02:19 -0500

    Se corrige modelo de productos

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -4,13 +4,12 @@ import {database} from '../config/database'
 export class ProductModel extends Model {
     public id: number;
     public user_id: number;
-    public discount_type_id: number;
     public uuid: string;
     public name: string;
     public price: string;
     public sku: string;
     public featured_image: string;
-    public discount_amount: string;
+    public discount_percent: string;
     public description: string;
     public status: number;
     public createdAt: string;
@@ -24,13 +23,12 @@ ProductModel.init({
         primaryKey: true
     },
     user_id: {type: DataTypes.INTEGER},
-    discount_type_id: {type: DataTypes.INTEGER},
     uuid: {type: DataTypes.STRING},
     name: {type: DataTypes.STRING},
     price: {type: DataTypes.DECIMAL},
     sku: {type: DataTypes.STRING},
     featured_image: {type: DataTypes.STRING},
-    discount_amount: {type: DataTypes.DECIMAL},
+    discount_percent: {type: DataTypes.DECIMAL},
     description: {type: DataTypes.STRING},
     status: {type: DataTypes.INTEGER},
     createdAt: {
